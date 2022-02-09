18e54072fb9db4d51095a92bcb038e795ae837e2
Juan Ignacio Rios <juanrios0495@gmail.com>
2022-02-08 23:37:58 -0300

    Refactor and comments

--- contracts/Crowfounding.sol
+++ contracts/Crowfounding.sol
@@ -2,6 +2,7 @@
  pragma solidity >=0.7.0 < 0.9.0;
 
  contract Crowfounding {
+     // Default: Opened (1)
      enum State { Closed, Opened }
 
      // Allow multiple projects
@@ -14,7 +15,7 @@
         string id;
         string name;
         string description;
-        State state; //= 1;  1 - Open, 0 - Closed
+        State state;
         uint founds;
         uint foundraisingGoal;
         address payable author;
@@ -23,45 +24,44 @@
 
      Project public project;
      
+     // Events
      event ProjectFunded(
          address from,
          uint amount,
          string projectId,
          string projectName
      );
-
      event ProjectStateChanged(
          string id,
          State newState
      );
 
+     // Handle errors
      error StatusNotDefined(int);
      error StatusIsClosed(int);
 
-
-     // Deprecated, only allow to add one project
-     //constructor(string memory _id, string memory _name, string memory _description, uint _foundraisingGoal) {
+     /* Deprecated, now we create multiple projects
+     constructor(string memory _id, string memory _name, string memory _description, uint _foundraisingGoal) {
          // clean up constructor
-         //project = Project(_id, _name, _description, State.Opened, 0, _foundraisingGoal, payable(msg.sender), msg.sender);
-         /* 
-         id = _id;
-         name = _name;
-         description = _description;
-         foundraisingGoal = _foundraisingGoal;
-         author = payable(msg.sender);
-         owner = msg.sender;
-         */
-     //}
+         project = Project(_id, _name, _description, State.Opened, 0, _foundraisingGoal, payable(msg.sender), msg.sender);
+         
+        //  id = _id;
+        //  name = _name;
+        //  description = _description;
+        //  foundraisingGoal = _foundraisingGoal;
+        //  author = payable(msg.sender);
+        //  owner = msg.sender;   
+     }*/
 
-     modifier onlyOwner() {
+     // Modifiers. Restrictions!
+     modifier isAuthor() {
          require(
              project.author == msg.sender,
              "Only the author can change the project state"
          );
          _;
      }
-
-      modifier onlyOthers() {
+      modifier isNotAuthor() {
          require(
              project.author != msg.sender,
              "The author can not add founds"
@@ -69,22 +69,37 @@
          _;
      }
 
-     function fundProject() public payable onlyOthers{
+     /**
+      * Method available for non-authors
+      */
+     function fundProject() public payable isNotAuthor {
+         // Check if the state is opened
          require(project.state == State.Opened, "The project is Closed.");
          require(msg.value > 0, "Amount must be greather than 0.");
+         // Transfer ehters
          project.author.transfer(msg.value);
+         // Accumulate founds
          project.founds += msg.value;
+         
+         // Shoot event, new project!
          emit ProjectFunded(msg.sender, msg.value, project.id, project.name);
      }
-
-     function changeProjectState(State newState) public onlyOwner{
+     /**
+      * The author can change the status
+      */
+     function changeProjectState(State newState) public isAuthor {
+         // Avoid invalid states
          require(newState == State.Opened || newState == State.Closed, "Invalid state");
+         // Avoid unnecessary change
          require(project.state != newState, "New state must be different");
+
          project.state = newState;
+         // Shoot event, updated state!
          emit ProjectStateChanged(project.id, newState);
      }
 
      function createProject() public {
-
+         // TODO: Implement method for create multiple projects. 
+         // Use Project[] array and mapping.
      }
  }
